f70ecc6d58920351e57af5863bdc48be804512e3
Michael D. Hoyle <hoylemd@gmail.com>
2016-08-14 13:41:33 -0400

    Images spritesheet

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,8 @@
-function create_room(room_type, room_size) {
-
-  var size_class = "room-slice-" + room_size;
-  var type_class = "room-" + room_type;
-  var $new_element = $('<div class="room-slice"></div>');
-
-  $new_element.addClass([size_class, type_class]);
-
-  return {
-    size: room_size,
-    type: room_type,
-    $element: $new_element,
-    level: null,
-    left: null
-  };
-}
-
-function create_level(number) {
-  var $new_element = $('<div class="vault-level"></div');
-
-  return {
-    depth: 0,
-    $element: $new_element,
-    rooms: [],
-    add_room: function(room, left) {
-
-    }
-  }
-}
-
-function create_vault() {
-  var $new_element = $('<div class="vault"></div');
-
-  return {
-    levels: [],
-    $element: $new_element,
-    add_level: function(level) {
-      level.depth = this.levels.length;
-      this.levels.append(level);
-    }
-  };
+var constants = {
+  TILE_COLUMNS: 6,
+  TILE_ROWS: 6,
+  TILE_WIDTH: 66,
+  TILE_HEIGHT: 66
 }
 
 function init_game(width, height, options) {
@@ -49,7 +13,10 @@ function init_game(width, height, options) {
   };
 
   //Create the renderer
-  var renderer = PIXI.autoDetectRenderer(width, height, options);
+  var renderer = PIXI.autoDetectRenderer(
+    constants.TILE_COLUMNS * constants.TILE_WIDTH,
+    constants.TILE_ROWS * constants.TILE_HEIGHT,
+    options);
   renderer.backgroundColor = 0x999999;
 
   //Add the canvas to the HTML document
@@ -68,55 +35,22 @@ function init_game(width, height, options) {
 }
 
 function main() {
-  var constants = {
-    VAULT_WIDTH: 832,
-    VAULT_HEIGHT: 640
-  }
-  var PI = 3.14159;
-  var room_dimensions = {
-    'height': 64,
-    'small': 96,
-    'medium': 192,
-    'large': 288
-  };
-  var room_coordinates = {
-    'red': 0,
-    'green': room_dimensions['height'],
-    'blue': 2 * room_dimensions['height'],
-    'small': 0,
-    'medium': room_dimensions['small'],
-    'large': room_dimensions['small'] + room_dimensions['medium']
-  };
+ var PI = 3.14159;
 
   // Alias some constructors
   var Sprite = PIXI.Sprite;
   var Rectangle = PIXI.Rectangle;
   var TextureCache = PIXI.utils.TextureCache;
 
-  function room_tile(size, colour) {
-    return new Rectangle(
-      room_coordinates[size], room_coordinates[colour],
-      room_dimensions[size], room_dimensions['height']
-    );
-  }
-
-  var game = init_game(constants.VAULT_WIDTH, constants.VAULT_HEIGHT);
+  var game = init_game(constants.TILE_COLUMNS, constants.TILE_ROWS);
 
   var textures = [
-    'assets/images/person.png',
-    'assets/images/room_sprites.png'
+    'assets/images/images.png',
   ]
 
   function done_loading() {
-    var entrance_texture = TextureCache['assets/images/room_sprites.png'];
-    entrance_texture.frame = room_tile('large', 'red');
-    var entrance_sprite = new Sprite(entrance_texture);
-    entrance_sprite.position.set(0, 0);
-    game.stage.addChild(entrance_sprite);
+    var person_sprite = new Sprite(TextureCache['assets/images/images.png']);
 
-    var person_sprite = new Sprite(TextureCache['assets/images/person.png']);
-
-    person_sprite.position.set(10, 5);
 
     game.stage.addChild(person_sprite);
     game.renderer.render(game.stage);
